bb99acfe24d1f2ce9b988b29ce62bdad13c93a56
ferry <kenny0326@qq.com>
2024-10-31 22:19:12 +0800

    Add Unitree-H1 env

--- RL_Policy/Create_a_Direct_Workflow_RL_Env/unitree_H1/__init__.py
+++ RL_Policy/Create_a_Direct_Workflow_RL_Env/unitree_H1/__init__.py
@@ -2,26 +2,26 @@
 @FileName：__init__.py
 @Description：
 @Author：Ferry
-@Time：2024/10/31 下午4:51
+@Time：2024/10/31 下午8:56
 @Copyright：©2024-2024 ShanghaiTech University-RIMLAB
 """
-""" Cartpole balancing environment. """
+""" Unitree-H1 balancing environment. """
 
 import gymnasium as gym
 
-from . import agent as agents
-from .direct_rl_scene_cartpole_env import CartpoleEnv, CartpoleEnvCfg
+from . import agents
+from .Unitree_H1 import UnitreeH1Env, UnitreeH1Cfg
 
 ##
 # Register Gym environments.
 ##
 
 gym.register(
-    id="Isaac-Cartpole-Direct-Mine-v0",
-    entry_point="Create_a_Direct_Workflow_RL_Env.cartpole:CartpoleEnv",
+    id="Isaac-UnitreeH1-Direct-v0",
+    entry_point="Create_a_Direct_Workflow_RL_Env.unitree_H1:UnitreeH1Env",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": CartpoleEnvCfg,
+        "env_cfg_entry_point": UnitreeH1Cfg,
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
     },
